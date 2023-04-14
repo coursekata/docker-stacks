1e133c0a346f49e41167f9b507bce414080dce38
Adam Blake <adamblake@g.ucla.edu>
2023-04-14 14:51:17 -0700

    chore(dev): add hadolint ignores; improve dev builder logic

--- dev-build.sh
+++ dev-build.sh
@@ -2,11 +2,13 @@
 
 set -e
 
+platforms=linux/amd64
+
 echo "Building python-notebook"
 docker buildx build python-notebook \
   --build-arg PYTHON_VERSION=3.10 \
   --build-arg BUILD_LAB=false \
-  --platform linux/amd64 \
+  --platform "${platforms}" \
   -t coursekata/python-notebook:test \
   "${@}"
 
@@ -14,21 +16,23 @@ echo "Building minimal-r-notebook"
 docker buildx build minimal-r-notebook \
   --build-arg PYTHON_VERSION=3.10 \
   --build-arg R_VERSION=4.2 \
-  --platform linux/amd64 \
+  --platform "${platforms}" \
   -t coursekata/minimal-r-notebook:test \
   "${@}"
 
 echo "Building r-notebook"
 docker buildx build r-notebook \
+  --build-arg BASE_IMAGE=coursekata/minimal-r-notebook \
   --build-arg BASE_TAG=test \
-  --platform linux/amd64 \
+  --platform "${platforms}" \
   -t coursekata/r-notebook:test \
   "${@}"
 
 echo "Building datascience-notebook"
 docker buildx build datascience-notebook \
+  --build-arg BASE_IMAGE=coursekata/r-notebook \
   --build-arg BASE_TAG=test \
   --build-arg BUILD_LAB=false \
-  --platform linux/amd64 \
+  --platform "${platforms}" \
   -t coursekata/datascience-notebook:test \
   "${@}"
