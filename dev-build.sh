bf8f338de23180189df85b765953780d895fde36
Adam Blake <adamblake@g.ucla.edu>
2023-04-17 09:55:35 -0700

    build(dev): parameterize build script; add test script

--- dev-build.sh
+++ dev-build.sh
@@ -2,37 +2,50 @@
 
 set -e
 
-platforms=linux/amd64
+images=${1:-all}
+platform=${2:-all}
+if [ "${platform}" = all ]; then
+  platform=linux/amd64,linux/arm64/v8 
+fi
+build_lab=${BUILD_LAB:-false}
 
-echo "Building python-notebook"
-docker buildx build python-notebook \
-  --build-arg PYTHON_VERSION=3.10 \
-  --build-arg BUILD_LAB=false \
-  --platform "${platforms}" \
-  -t coursekata/python-notebook:test \
-  "${@}"
+if [ "${images}" = all ] || [ "${images}" = python-notebook ]; then
+  image=python-notebook
+  echo "Building ${image}"
+  docker buildx build "${image}" \
+    --build-arg PYTHON_VERSION=3.10 \
+    --build-arg BUILD_LAB="${build_lab}" \
+    --platform "${platform}" \
+    -t coursekata/"${image}":test
+fi
 
-echo "Building minimal-r-notebook"
-docker buildx build minimal-r-notebook \
-  --build-arg PYTHON_VERSION=3.10 \
-  --build-arg R_VERSION=4.2 \
-  --platform "${platforms}" \
-  -t coursekata/minimal-r-notebook:test \
-  "${@}"
+if [ "${images}" = all ] || [ "${images}" = minimal-r-notebook ] || [ "${images}" = r-notebook ] || [ "${images}" = datascience-notebook ]; then
+  image=minimal-r-notebook
+  echo "Building ${image}"
+  docker buildx build "${image}" \
+    --build-arg PYTHON_VERSION=3.10 \
+    --build-arg R_VERSION=4.2 \
+    --platform "${platform}" \
+    -t coursekata/"${image}":test
+fi
 
-echo "Building r-notebook"
-docker buildx build r-notebook \
-  --build-arg BASE_IMAGE=coursekata/minimal-r-notebook \
-  --build-arg BASE_TAG=test \
-  --platform "${platforms}" \
-  -t coursekata/r-notebook:test \
-  "${@}"
+if [ "${images}" = all ] || [ "${images}" = r-notebook ] || [ "${images}" = datascience-notebook ]; then
+  image=r-notebook
+  echo "Building ${image}"
+  docker buildx build "${image}" \
+    --build-arg BASE_IMAGE=coursekata/minimal-r-notebook \
+    --build-arg BASE_TAG=test \
+    --platform "${platform}" \
+    -t coursekata/"${image}":test
+fi
 
-echo "Building datascience-notebook"
-docker buildx build datascience-notebook \
-  --build-arg BASE_IMAGE=coursekata/r-notebook \
-  --build-arg BASE_TAG=test \
-  --build-arg BUILD_LAB=false \
-  --platform "${platforms}" \
-  -t coursekata/datascience-notebook:test \
-  "${@}"
+if [ "${images}" = all ] || [ "${images}" = datascience-notebook ]; then
+  image=datascience-notebook
+  echo "Building ${image}"
+  docker buildx build "${image}" \
+    --build-arg BASE_IMAGE=coursekata/r-notebook \
+    --build-arg BASE_TAG=test \
+    --build-arg BUILD_LAB="${build_lab}" \
+    --platform "${platform}" \
+    -t coursekata/"${image}":test
+fi
---
+++ dev-test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+image=${1:-minimal-r-notebook}
+platform=${DEV_PLATFORM:-${2:-linux/arm64/v8}}
+
+./dev-build.sh "${image}" "${platform}"
+docker run -p 8888:8888 --platform "${platform}" coursekata/"${image}":test
