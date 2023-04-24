33bde73561b3496767c43b3bb7a1f05b8f03433a
Adam Blake <adamblake@g.ucla.edu>
2023-04-24 15:13:24 -0700

    feat: use correct org to take advantage of caching

--- dev-build.sh
+++ dev-build.sh
@@ -5,8 +5,11 @@ set -e
 images=${1:-all}
 platform=${2:-all}
 if [ "${platform}" = all ]; then
-  platform=linux/amd64,linux/arm64/v8 
+  platform="linux/amd64,linux/arm64/v8"
 fi
+
+org="ghcr.io/coursekata"
+tag="test"
 
 with-margin() {
   echo ""
@@ -17,24 +20,24 @@ with-margin() {
 image=base-r-notebook
 with-margin echo "Building ${image}"
 docker buildx build "${image}" \
-  -t coursekata/"${image}":test \
+  -t "${org}/${image}":test \
   --platform "${platform}" \
   --build-context scripts=scripts \
   --build-arg PYTHON_VERSION=3.10 \
   --build-arg R_VERSION=4.2
-with-margin docker images coursekata/"${image}":test
+with-margin docker images "${org}/${image}:${tag}"
 
 dependents=("all" "essentials-builder" "essentials-notebook" "r-notebook" "datascience-notebook")
 if [[ " ${dependents[*]} " =~ (^|[[:space:]])${images}($|[[:space:]]) ]]; then
   image=essentials-builder
   with-margin echo "Building ${image}"
   docker buildx build "${image}" \
-    -t coursekata/"${image}":test \
+    -t "${org}/${image}:${tag}" \
     --platform "${platform}" \
     --build-context scripts=scripts \
-    --build-arg ORG=coursekata \
-    --build-arg BASE_TAG=test
-  with-margin docker images coursekata/"${image}":test
+    --build-arg ORG=${org} \
+    --build-arg BASE_TAG=${tag}
+  with-margin docker images "${org}/${image}:${tag}"
 fi
 
 dependents=("all" "essentials-notebook" "datascience-notebook")
@@ -42,12 +45,12 @@ if [[ " ${dependents[*]} " =~ (^|[[:space:]])${images}($|[[:space:]]) ]]; then
   image=essentials-notebook
   with-margin echo "Building ${image}"
   docker buildx build "${image}" \
-    -t coursekata/"${image}":test \
+    -t "${org}/${image}:${tag}" \
     --platform "${platform}" \
     --build-context scripts=scripts \
-    --build-arg ORG=coursekata \
-    --build-arg BASE_TAG=test
-  with-margin docker images coursekata/"${image}":test
+    --build-arg ORG=${org} \
+    --build-arg BASE_TAG=${tag}
+  with-margin docker images "${org}/${image}:${tag}"
 fi
 
 dependents=("all" "r-notebook" "datascience-notebook")
@@ -55,12 +58,12 @@ if [[ " ${dependents[*]} " =~ (^|[[:space:]])${images}($|[[:space:]]) ]]; then
   image=r-notebook
   with-margin echo "Building ${image}"
   docker buildx build "${image}" \
-    -t coursekata/"${image}":test \
+    -t "${org}/${image}:${tag}" \
     --platform "${platform}" \
     --build-context scripts=scripts \
-    --build-arg ORG=coursekata \
-    --build-arg BASE_TAG=test
-  with-margin docker images coursekata/"${image}":test
+    --build-arg ORG=${org} \
+    --build-arg BASE_TAG=${tag}
+  with-margin docker images "${org}/${image}:${tag}"
 fi
 
 dependents=("all" "datascience-notebook")
@@ -68,9 +71,9 @@ if [[ " ${dependents[*]} " =~ (^|[[:space:]])${images}($|[[:space:]]) ]]; then
   image=datascience-notebook
   with-margin echo "Building ${image}"
   docker buildx build "${image}" \
-    -t coursekata/"${image}":test \
+    -t "${org}/${image}:${tag}" \
     --platform "${platform}" \
-    --build-arg ORG=coursekata \
-    --build-arg BASE_TAG=test
-  with-margin docker images coursekata/"${image}":test
+    --build-arg ORG=${org} \
+    --build-arg BASE_TAG=${tag}
+  with-margin docker images "${org}/${image}:${tag}"
 fi
