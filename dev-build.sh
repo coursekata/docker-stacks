de5cbc778256649675b73f7b20d929089d5c3ad3
Adam Blake <adamblake@g.ucla.edu>
2023-04-24 16:26:06 -0700

    feat: configure dev builder to use same tags as GH

--- dev-build.sh
+++ dev-build.sh
@@ -9,7 +9,8 @@ if [ "${platform}" = all ]; then
 fi
 
 org="ghcr.io/coursekata"
-tag="test"
+r_version="4.2"
+python_version="3.10"
 
 with-margin() {
   echo ""
@@ -20,24 +21,29 @@ with-margin() {
 image=base-r-notebook
 with-margin echo "Building ${image}"
 docker buildx build "${image}" \
-  -t "${org}/${image}":test \
+  -t "${org}/${image}:latest" \
+  -t "${org}/${image}:sha-$(git rev-parse --short=8 HEAD)" \
+  -t "${org}/${image}:r-${r_version}" \
+  -t "${org}/${image}:python-${python_version}" \
   --platform "${platform}" \
   --build-context scripts=scripts \
-  --build-arg PYTHON_VERSION=3.10 \
-  --build-arg R_VERSION=4.2
-with-margin docker images "${org}/${image}:${tag}"
+  --build-arg PYTHON_VERSION=${python_version} \
+  --build-arg R_VERSION=${r_version}
+with-margin docker images "${org}/${image}:sha-$(git rev-parse --short=8 HEAD)"
 
 dependents=("all" "essentials-builder" "essentials-notebook" "r-notebook" "datascience-notebook")
 if [[ " ${dependents[*]} " =~ (^|[[:space:]])${images}($|[[:space:]]) ]]; then
   image=essentials-builder
   with-margin echo "Building ${image}"
   docker buildx build "${image}" \
-    -t "${org}/${image}:${tag}" \
+    -t "${org}/${image}:latest" \
+    -t "${org}/${image}:sha-$(git rev-parse --short=8 HEAD)" \
+    -t "${org}/${image}:r-${r_version}" \
+    -t "${org}/${image}:python-${python_version}" \
     --platform "${platform}" \
     --build-context scripts=scripts \
-    --build-arg ORG=${org} \
-    --build-arg BASE_TAG=${tag}
-  with-margin docker images "${org}/${image}:${tag}"
+    --build-arg BASE_TAG="sha-$(git rev-parse --short=8 HEAD)"
+  with-margin docker images "${org}/${image}:sha-$(git rev-parse --short=8 HEAD)"
 fi
 
 dependents=("all" "essentials-notebook" "datascience-notebook")
@@ -45,12 +51,14 @@ if [[ " ${dependents[*]} " =~ (^|[[:space:]])${images}($|[[:space:]]) ]]; then
   image=essentials-notebook
   with-margin echo "Building ${image}"
   docker buildx build "${image}" \
-    -t "${org}/${image}:${tag}" \
+    -t "${org}/${image}:latest" \
+    -t "${org}/${image}:sha-$(git rev-parse --short=8 HEAD)" \
+    -t "${org}/${image}:r-${r_version}" \
+    -t "${org}/${image}:python-${python_version}" \
     --platform "${platform}" \
     --build-context scripts=scripts \
-    --build-arg ORG=${org} \
-    --build-arg BASE_TAG=${tag}
-  with-margin docker images "${org}/${image}:${tag}"
+    --build-arg BASE_TAG="sha-$(git rev-parse --short=8 HEAD)"
+  with-margin docker images "${org}/${image}:sha-$(git rev-parse --short=8 HEAD)"
 fi
 
 dependents=("all" "r-notebook" "datascience-notebook")
@@ -58,12 +66,14 @@ if [[ " ${dependents[*]} " =~ (^|[[:space:]])${images}($|[[:space:]]) ]]; then
   image=r-notebook
   with-margin echo "Building ${image}"
   docker buildx build "${image}" \
-    -t "${org}/${image}:${tag}" \
+    -t "${org}/${image}:latest" \
+    -t "${org}/${image}:sha-$(git rev-parse --short=8 HEAD)" \
+    -t "${org}/${image}:r-${r_version}" \
+    -t "${org}/${image}:python-${python_version}" \
     --platform "${platform}" \
     --build-context scripts=scripts \
-    --build-arg ORG=${org} \
-    --build-arg BASE_TAG=${tag}
-  with-margin docker images "${org}/${image}:${tag}"
+    --build-arg BASE_TAG="sha-$(git rev-parse --short=8 HEAD)"
+  with-margin docker images "${org}/${image}:sha-$(git rev-parse --short=8 HEAD)"
 fi
 
 dependents=("all" "datascience-notebook")
@@ -71,9 +81,11 @@ if [[ " ${dependents[*]} " =~ (^|[[:space:]])${images}($|[[:space:]]) ]]; then
   image=datascience-notebook
   with-margin echo "Building ${image}"
   docker buildx build "${image}" \
-    -t "${org}/${image}:${tag}" \
+    -t "${org}/${image}:latest" \
+    -t "${org}/${image}:sha-$(git rev-parse --short=8 HEAD)" \
+    -t "${org}/${image}:r-${r_version}" \
+    -t "${org}/${image}:python-${python_version}" \
     --platform "${platform}" \
-    --build-arg ORG=${org} \
-    --build-arg BASE_TAG=${tag}
-  with-margin docker images "${org}/${image}:${tag}"
+    --build-arg BASE_TAG="sha-$(git rev-parse --short=8 HEAD)"
+  with-margin docker images "${org}/${image}:sha-$(git rev-parse --short=8 HEAD)"
 fi
