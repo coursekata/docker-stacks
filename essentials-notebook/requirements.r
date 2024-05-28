113baa39de578360b38e819d567d0c82b98370a8
Adam Blake <adam@ablake.dev>
2024-05-28 12:14:30 -0700

    fix(essentials): explicitly install coursekata soft and remote deps.

--- essentials-notebook/requirements.r
+++ essentials-notebook/requirements.r
@@ -1,11 +1,13 @@
 options(Ncpus = max(1L, parallel::detectCores()))
 
 remotes::install_github(c(
-  "coursekata/coursekata-r",
-  "coursekata/testwhat"
+  "coursekata/testwhat",
+  "fivethirtyeightdata/fivethirtyeightdata",
+  "rpruim/Lock5withR"
 ))
 
 remotes::install_cran(c(
+  "coursekata",
   "fivethirtyeight",
   "ggpubr",
   "gridExtra",
