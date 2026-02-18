bd957283daf37d16d942ce6dc745057ac7d14325
Adam Blake <adam@coursekata.org>
2026-02-18 12:21:03 -0800

    fix: map jupyterhub-singleuser to jupyterhub for PyPI name consistency

--- scripts/list-python-packages.sh
+++ scripts/list-python-packages.sh
@@ -41,6 +41,7 @@ pixi_json=$(pixi list -e "$ENVIRONMENT" --json) || exit 1
 packages=$(
   echo "$pixi_json" \
     | jq -r '.[] | select(.is_explicit) | select(.name | test("^(r-|python$|pip$|unixodbc$|cmdstan$)") | not) | .name' \
+    | sed 's/^jupyterhub-singleuser$/jupyterhub/' \
     | sort -u
 )
 
